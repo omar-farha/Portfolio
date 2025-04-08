dceb7aa3501fbfd20d646bd1ce8ee1681fcb1f1d
omar-farha <farha.omar2008@gmail.com>
2025-04-08 22:52:47 +0200

    fixing second new error

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -163,7 +163,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e: Position) => e.color)
+      .arcColor((e: unknown) => (e as Position).color) // Fixed line
       .arcAltitude((e) => (e as { arcAlt: number }).arcAlt * 1)
       .arcStroke(() => [0.32, 0.28, 0.3][Math.round(Math.random() * 2)])
       .arcDashLength(defaultProps.arcLength)
